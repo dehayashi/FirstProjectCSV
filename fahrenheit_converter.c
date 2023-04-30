274791111c40ec752630515fc1e0af6570a1e5db
DE HAYASHI <126200204+dehayashi@users.noreply.github.com>
2023-04-30 15:57:22 -0300

    Alteração final do projeto

--- fahrenheit_converter.c
+++ fahrenheit_converter.c
@@ -3,8 +3,37 @@
 int main() {
     float fahrenheit, celsius;
     printf("Enter temperature in Fahrenheit: ");
-    scanf("%f", &fahrenheit);
+    if (scanf("%f", &fahrenheit) != 1) {
+        printf("Invalid input.");
+        return 1;
+    }
+    if (fahrenheit < -459.67) {
+        printf("Temperature is too low.");
+        return 1;
+    }
+    if (fahrenheit > 100000000) {
+        printf("Temperature is too high.");
+        return 1;
+    }
     celsius = (fahrenheit - 32) * 5 / 9;
-    printf("%.2f Fahrenheit = %.2f Celsius", fahrenheit, celsius);
+    printf("%.2f Fahrenheit = %.2f Celsius\n", fahrenheit, celsius);
+
+    // optional: add functionality to convert Celsius to Fahrenheit
+    printf("Enter temperature in Celsius: ");
+    if (scanf("%f", &celsius) != 1) {
+        printf("Invalid input.");
+        return 1;
+    }
+    if (celsius < -273.15) {
+        printf("Temperature is too low.");
+        return 1;
+    }
+    if (celsius > 100000000) {
+        printf("Temperature is too high.");
+        return 1;
+    }
+    fahrenheit = celsius * 9 / 5 + 32;
+    printf("%.2f Celsius = %.2f Fahrenheit\n", celsius, fahrenheit);
+
     return 0;
 }
